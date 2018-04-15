b0e6065b4e24c8d92b45533db0732a17ec048272
546417331@qq.com <546417331@qq.com>
2018-04-15 16:12:44 +0800

    modify layout

--- js/app.js
+++ js/app.js
@@ -1,25 +1,46 @@
-function initMap(){
-    var map = new BMap.Map("map");
-    // 创建地图实例  
-    var point = new BMap.Point(116.404, 39.915);
-    // 创建点坐标  
-    map.enableScrollWheelZoom();
-    //开启鼠标滚动缩放功能
-    map.centerAndZoom(point, 15);
-
+// 创建地图实例  
+const map = new BMap.Map("map");
+/**
+ * @description  添加地点标记
+ * @param {BMap.point} point
+ */
+function addMarker(point) {
     var marker = new BMap.Marker(point);
-    marker.enableDragging();
-    marker.addEventListener('click',function(){
-        var opts = {    
-            width : 250,     // 信息窗口宽度    
-            height: 100,     // 信息窗口高度    
-            title : "Hello"  // 信息窗口标题   
-        }    
-        var infoWindow = new BMap.InfoWindow("World", opts);  // 创建信息窗口对象    
-        map.openInfoWindow(infoWindow, map.getCenter());      // 打开信息窗口
-    })
     map.addOverlay(marker);
-
+}
+/**
+ * @description 初始化地图
+ */
+function initMap() {
+    // 创建点坐标  
+    let point = new BMap.Point(116.404, 39.915);
+    //开启鼠标滚动缩放功能
+    map.enableScrollWheelZoom();
+    map.addControl(new BMap.NavigationControl());
+    map.centerAndZoom(point, 15);
+    // addMarker(point);
+    let local = new BMap.LocalSearch(map, {
+        renderOptions: {
+            map: map
+        }
+    });
+    local.search("学校");
+}
+/**
+ * @description 主页侧边栏显示隐藏
+ */
+function filterToggle() {
+    //侧边栏处于显示状态则隐藏
+    if ($("div#left").hasClass("show")) {
+        $("div#left").removeClass("show");
+        $("div#right").removeClass();
+        $("div#right").addClass("col-lg-12");
+    } else { //侧边栏处于隐藏状态则显示
+        $("div#right").removeClass();
+        $("div#right").addClass("col-lg-10");
+        $("div#left").addClass("show");
+    }
+    // const left = documentGetElementById("#left");
 }
 // let map;
 
@@ -60,6 +81,10 @@ function initMap(){
 // };
 
 // ko.applyBindings(new WebmailViewModel());
+/**
+ * @description jquery调用初始化页面
+ */
 $(document).ready(function () {
+    //初始化地图
     initMap();
 });
