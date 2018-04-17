c9e9b0f3962e6ec51fbe7aae5ee1fc834e511401
546417331@qq.com <546417331@qq.com>
2018-04-17 09:06:33 +0800

    finish

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 const addressCapacity = 30; //地点数量
 let markerArray = [];
+const map = new BMap.Map("map"); // 创建地图实例  
+let infoWindow;  //标记窗口
+
 /**
  * @description  添加地点标记
  * @param {BMap.point} point
@@ -14,38 +17,37 @@ function addMarker(address, map) {
     }
     //监听事件--点击marker打开信息窗口
     marker.addEventListener("click", function () {
-        let infoWindow = new BMap.InfoWindow(address.address, opts); // 创建信息窗口对象    
+        infoWindow = new BMap.InfoWindow(address.address, opts); // 创建信息窗口对象    
         map.openInfoWindow(infoWindow, address.point); // 打开信息窗口
     });
     //加载marker
+    markerArray.push({"title":address.title,"marker":marker});
     map.addOverlay(marker);
-    // console.log(marker.point);
-    // marker.setAnimation(BMAP_ANIMATION_BOUNCE);
     // map.removeOverlay(marker);
 }
+
 /**
  * @description 初始化地图
  */
 function initMap() {
     new Promise(function (resolve, reject) {
         let self = this;
-        const map = new BMap.Map("map"); // 创建地图实例  
         // 创建点坐标  北京天安门
         let point = new BMap.Point(116.404, 39.915);
         //开启鼠标滚动缩放功能
         map.enableScrollWheelZoom();
         map.addControl(new BMap.NavigationControl());
         map.centerAndZoom(point, 12);
-        // addMarker(point);
         //回调函数，获取数据结果
         const options = {
             onSearchComplete: function (results) {
-                if (local.getStatus() == BMAP_STATUS_SUCCESS) {
-                    console.log(typeof(results.zr));
-                    ko.applyBindings(new addressVM(results.zr));
+                if (local.getStatus() == BMAP_STATUS_SUCCESS) {  //成功获取数据
+                    //显示地点标记
                     results.zr.map((address) => {
                         addMarker(address, map)
                     });
+                    //绑定数据
+                    ko.applyBindings(new addressVM(markerArray));
                 } else {
                     alert("获取数据失败！");
                 }
@@ -54,15 +56,61 @@ function initMap() {
         };
         let local = new BMap.LocalSearch(map, options);
         local.search("学校");
-        resolve("markerArray");
-    }).then(function(p){
-        console.log(p);
+        resolve();
     }).catch(function(reason){
         alert("地图加载失败！");
     });
-
-
 }
+
+/**
+ * @description  clickItem  点击动画事件
+ */
+function clickItem(marker){
+    marker.setAnimation(BMAP_ANIMATION_BOUNCE);  //标记动画--弹跳
+    setTimeout(marker.setAnimation(null),2000);  //2s后取消动画
+}
+/**
+ * @description updateMarker(newItem)
+ */
+function updateMarker(newItem){
+    map.clearOverlays();
+    newItem.map((item)=>{map.addOverlay(item.marker)})
+}
+/**
+ * @description addressVM 地址列表控制器
+ */
+function addressVM(data) {
+    let self = this;
+    self.mArray = data.concat();
+    //过滤关键字
+    self.keyWords = ko.observable();
+    // 地址列表
+    self.address = ko.observableArray(self.mArray);
+    // 过滤查找
+    self.addressfilter = function () {
+        if($.trim(self.keyWords())==="") { //查找关键字为空
+            alert("给个关键字呗！");
+        }else{
+            // 更新左侧列表
+            const oldItem = markerArray.slice();
+            let newItem = oldItem.filter((data)=>(data.title.match($.trim(self.keyWords()))));
+            self.address.removeAll();
+            newItem.map((item)=>{self.address.push(item)})
+            // 更新右侧地址标记
+            updateMarker(newItem);
+        }
+    };
+    // 列表项点击事件
+    self.showPoint = function(p){
+        clickItem(p.marker);
+    }
+}
+
+// 加载页面
+$(document).ready(function () {
+    initMap();//初始化地图
+});
+
 /**
  * @description 主页侧边栏显示隐藏
  */
@@ -78,64 +126,3 @@ function filterToggle() {
         $("div#left").addClass("show");
     }
 }
-
-// function initMap() {
-//     //
-//     map = new google.maps.Map(document.getElementById('map'), {
-//         center: {
-//             lat: 37.884,
-//             lng: 112.558
-//         },
-//         zoom: 15
-//     });
-//     let sydney = {
-//         lat: -34.397,
-//         lng: 150.644
-//     };
-//     let marker = new google.maps.Marker({
-//         position: sydney,
-//         map: map,
-//         title: 'First marker'
-//     });
-//     let infoWindow = new google.maps.InfoWindow({
-//         content: "悉尼"
-//     });
-//     marker.addListener('click', function () {
-//         infoWindow.open(map, marker);
-//     })
-// }
-
-// function WebmailViewModel() {
-//     // Data
-//     var self = this;
-//     self.folders = ['Inbox', 'Archive', 'Sent', 'Spam'];
-//     self.chosenFolderId = ko.observable();
-
-//     // Behaviours
-//     self.goToFolder = function(folder) { self.chosenFolderId(folder); };
-// };
-
-// ko.applyBindings(new WebmailViewModel());
-
-/**
- * @description addressVM 地址列表控制器
- */
-function addressVM(data) {
-    let self = this;
-    this.showList = data.map(data=> {return {title:data.title}});
-    this.keyWords = ko.observable();
-    self.address = ko.observableArray(self.showList);
-    this.addressSearch = function () {
-        console.log(self.keyWords());
-    };
-    // console.log(self.showList);
-}
-
-
-/**
- * @description jquery调用初始化页面
- */
-$(document).ready(function () {
-    //初始化地图
-    initMap();
-});
